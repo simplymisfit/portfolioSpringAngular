5e2e0d5e2b77d2c0c864ac17ff1c38d8b242ab0c
msft <63207978+simplymisfit@users.noreply.github.com>
2022-03-21 11:21:30 +0100

    Initial commit

---
+++ src/main/java/pl/rosochacki/portfolioSpringAngular/PortfolioSpringAngularApplication.java
@@ -0,0 +1,13 @@
+package pl.rosochacki.portfolioSpringAngular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioSpringAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioSpringAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/rosochacki/portfolioSpringAngular/PortfolioSpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rosochacki.portfolioSpringAngular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioSpringAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
