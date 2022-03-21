9d4515036a86f27d9e7267e848334a804ef7dc02
msft <63207978+simplymisfit@users.noreply.github.com>
2022-03-21 13:25:28 +0100

    x

--- angular-portfolio/angular-portfolio/src/app/app.module.ts
+++ angular-portfolio/angular-portfolio/src/app/app.module.ts
@@ -5,10 +5,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { PortfolioService } from './services/portfolio.service';
 import { PortfolioListComponent } from './components/portfolio-list/portfolio-list.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  { path: 'portfolio/:id', component: PortfolioListComponent },
+  { path: 'portfolio', component: PortfolioListComponent },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
+];
 @NgModule({
   declarations: [AppComponent, PortfolioListComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [RouterModule.forRoot(routes), BrowserModule, HttpClientModule],
   providers: [PortfolioService],
   bootstrap: [AppComponent],
 })
