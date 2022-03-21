6ad192a5ec6cd2a24be3590bc578d20dd331b7d1
msft <63207978+simplymisfit@users.noreply.github.com>
2022-03-21 13:14:44 +0100

    x

--- portfolioSpringAngular/src/main/java/pl/rosochacki/portfolioSpringAngular/dao/PortfolioRepository.java
+++ portfolioSpringAngular/src/main/java/pl/rosochacki/portfolioSpringAngular/dao/PortfolioRepository.java
@@ -1,7 +1,9 @@
 package pl.rosochacki.portfolioSpringAngular.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import pl.rosochacki.portfolioSpringAngular.entity.Portfolio;
 
+@CrossOrigin("http://localhost:4200")
 public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
 }
