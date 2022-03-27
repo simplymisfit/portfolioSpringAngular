d9b42f80bf8e4f4e59ce508c0873193f67f389db
msft <63207978+simplymisfit@users.noreply.github.com>
2022-03-27 18:45:42 +0200

    x

--- angular-portfolio/angular-portfolio/src/app/app.component.ts
+++ angular-portfolio/angular-portfolio/src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import {
+  faAddressBook,
+  faFile,
+  faHouse,
+  faWindowMaximize,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +16,8 @@ export class AppComponent {
   title = 'angular-portfolio';
   faGithub = faGithub;
   faLinkedin = faLinkedin;
+  faHouse = faHouse;
+  faWindowMaximize = faWindowMaximize;
+  faFile = faFile;
+  faAddressBook = faAddressBook;
 }
